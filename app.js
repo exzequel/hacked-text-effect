12f309ed377d91dd1bf35ab8845d70b998a5eb53
Exequel <exql.adzn@gmail.com>
2023-02-18 17:52:09 +0800

    add style in body center h1

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-const letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 document.querySelector("h1").onmouseover = event => {
     event.target.innerText = event.target.innerText.split("").map(letters[Math.floor(Math.random() * 26)]).join("");
