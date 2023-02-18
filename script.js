8e1cb73dd650a709aa5623fe9d892f9e95b2ba36
Exequel <exql.adzn@gmail.com>
2023-02-18 18:27:19 +0800

    make text random at first then shift to original

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 document.querySelector("h1").onmouseover = event => {
     let iterations = 0;
 
-    const interval = setInterval((){
+    const interval = setInterval(() => {
         event.target.innerText = event.target.innerText.split("")
-            .map(letter = letters[Math.floor(Math.random() * 26)])
-            .join("");
+        .map((letter, index) => {
+            if(index < iterations) {
+                return event.target.dataset.value[index];
+            }
+            return letters[Math.floor(Math.random() * 26)]
+        }) 
+        .join("");
 
         if(iterations >= 9) clearInterval(interval);
         iterations += 1;
