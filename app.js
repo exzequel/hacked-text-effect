520099d026c0de1b7a5757ec55152cd7ad5ac333
Exequel <exql.adzn@gmail.com>
2023-02-18 17:45:53 +0800

    add letter changing effect

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 document.querySelector("h1").onmouseover = event => {
-    event.target.innerText = letters[Math.floor(Math.random() * 26)];
+    event.target.innerText = event.target.innerText.split("").map(letters[Math.floor(Math.random() * 26)]).join("");
 }
