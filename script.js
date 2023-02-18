90616bab27c991bb007c4339437fbe28b4fdff94
Exequel <exql.adzn@gmail.com>
2023-02-18 18:16:42 +0800

    made text not loop infinitely

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 document.querySelector("h1").onmouseover = event => {
-    setInterval((){
+    let iterations = 0;
+
+    const interval = setInterval((){
         event.target.innerText = event.target.innerText.split("")
             .map(letter = letters[Math.floor(Math.random() * 26)])
             .join("");
+
+        if(iterations >= 9) clearInterval(interval);
+        iterations += 1;
     }, 30)
 }
 
