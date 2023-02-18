e35ce08e4391be502a6f1a152c44e312b0167388
Exequel <exql.adzn@gmail.com>
2023-02-18 18:13:25 +0800

    add interval to show text changing

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 document.querySelector("h1").onmouseover = event => {
-    event.target.innerText = event.target.innerText.split("").map(letter = letters[Math.floor(Math.random() * 26)]).join("");
+    setInterval((){
+        event.target.innerText = event.target.innerText.split("")
+            .map(letter = letters[Math.floor(Math.random() * 26)])
+            .join("");
+    }, 30)
 }
 
